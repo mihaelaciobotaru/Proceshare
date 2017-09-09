c0be5db0bd2427e520554e6793768575fa630ed7
Mihaela Ciobotaru <mihaela.gabriela.93@gmail.com>
2017-09-09 12:32:12 +0300

    Main-design

---
+++ Proceshare/Proceshare.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Proceshare
+{
+    partial class Proceshare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.calculate = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.calculate);
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(460, 374);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(140, 201);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(183, 63);
+            this.calculate.TabIndex = 1;
+            this.calculate.Text = "Start calculating";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // Proceshare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 370);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Proceshare";
+            this.Text = "Proceshare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button calculate;
+    }
+}
+
--- Proceshare/Program.cs
+++ Proceshare/Program.cs
@@ -16,7 +16,7 @@ namespace Proceshare
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Proceshare());
         }
     }
 }
